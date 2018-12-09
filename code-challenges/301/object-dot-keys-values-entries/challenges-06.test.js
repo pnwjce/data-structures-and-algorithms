1f9422ed2be162b7bcc8902eb76fd26d75ccdcee
pnwjce <elliott.jess@gmail.com>
2018-12-08 17:59:18 -0800

    adding files

--- code-challenges/301/object-dot-keys-values-entries/challenges-06.test.js
+++ code-challenges/301/object-dot-keys-values-entries/challenges-06.test.js
@@ -120,7 +120,6 @@ const hasChildrenEntries = (arr, character) => {
   // Solution code here...
   for(let i in arr){
     if(character === arr[i].name){
-      console.log(Object.entries(arr[i])[2]);
       let babies = Object.entries(arr[i])[2];
       if(babies[1].length === 0){
         return false;
@@ -143,6 +142,7 @@ For example: [{ house: 'Stark', members: 7 }, { house: 'Arryn', members: 3 }, ..
 const houseSize = (arr) => {
   const sizes = [];
   // Solution code here...
+
   return sizes;
 };
 
@@ -210,12 +210,12 @@ describe('Testing challenge 5', () => {
   });
 });
 
-// describe('Testing challenge 6', () => {
-//   test('It should return an object for each house containing the name and size', () => {
-//     expect(houseSize(characters)).toStrictEqual([ { house: 'Stark', members: 7 }, { house: 'Arryn', members: 3 }, { house: 'Lannister', members: 5 }, { house: 'Targaryen', members: 5 }, { house: 'Tyrell', members: 4 }, { house: 'Stark', members: 2 }, { house: 'Snow', members: 1 } ]);
-//     expect(houseSize(characters).length).toStrictEqual(7);
-//   });
-// });
+describe('Testing challenge 6', () => {
+  test('It should return an object for each house containing the name and size', () => {
+    expect(houseSize(characters)).toStrictEqual([ { house: 'Stark', members: 7 }, { house: 'Arryn', members: 3 }, { house: 'Lannister', members: 5 }, { house: 'Targaryen', members: 5 }, { house: 'Tyrell', members: 4 }, { house: 'Stark', members: 2 }, { house: 'Snow', members: 1 } ]);
+    expect(houseSize(characters).length).toStrictEqual(7);
+  });
+});
 
 // describe('Testing challenge 7', () => {
 //   test('It should not include any deceased spouses', () => {
