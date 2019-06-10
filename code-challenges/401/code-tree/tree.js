09ead2f39349a91db1a69ab48f268b0fdbdfad86
pnwjce <elliott.jess@gmail.com>
2019-06-10 01:53:52 -0700

    code trees

---
+++ code-challenges/401/code-tree/tree.js
@@ -0,0 +1,95 @@
+'use strict';
+
+class Node {
+
+    constructor (value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+}
+
+class Tree {
+
+    constructor () {
+        this.root = null;
+    }
+
+    preOrder () {
+
+        let nodes = [];
+
+        function _walk (node) {
+
+            nodes.push(node.value);
+
+            _walk(this.root);
+
+            if (node.left) {
+                _walk(node.left);
+            }
+
+            if (node.right) {
+                _walk(node.right);
+            }
+
+            return nodes;
+
+        }
+
+    }
+
+    inOrder () {
+
+        let nodes = [];
+
+        function _walk (node) {
+
+            nodes.push(node.value);
+
+
+            if (node.left) {
+                _walk(node.left);
+            }
+
+            _walk(this.root);
+
+            if (node.right) {
+                _walk(node.right);
+            }
+
+            return nodes;
+
+        }
+
+    }
+
+    postOrder () {
+
+        let nodes = [];
+
+        function _walk (node) {
+
+            nodes.push(node.value);
+
+
+            if (node.left) {
+                _walk(node.left);
+            }
+
+
+            if (node.right) {
+                _walk(node.right);
+            }
+
+            _walk(this.root);
+
+            return nodes;
+
+        }
+
+    }
+
+}
+
